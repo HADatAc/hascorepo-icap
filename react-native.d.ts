959336c4d8dd348d7ca8aea83b4edc19567886be
Rui Durao <rui.durao@ipiaget.pt>
2025-03-10 00:32:21 +0000

    First steps ongoing

---
+++ react-native.d.ts
@@ -0,0 +1,7 @@
+declare module 'react-native/Libraries/NewAppScreen' {
+    export const Colors: any;
+    export const DebugInstructions: any;
+    export const Header: any;
+    export const LearnMoreLinks: any;
+    export const ReloadInstructions: any;
+  }
